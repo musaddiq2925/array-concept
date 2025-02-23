61a22ba48a0899ba46ac7fa4bcbd6877756f6a4f
nourinCT <nourin.ct@gmail.com>
2025-02-23 23:14:18 +0600

    variable using array

---
+++ array.js
@@ -0,0 +1,17 @@
+/**
+ * Using Variable Array
+*/
+
+// const num1 = 12;
+// const num2 = 13;
+// const num3 = 14;
+// const num4 = 15;
+// const num5 = 16;
+
+// const numbers = 111333222999;
+// (), {}, []
+
+const numbers = [12, 13, 14, 15, 16];
+const frands = ['Pagol', 'Fokir', 'Miskin', 'Atim', 'Gundda'];
+const passed = [true, false, true, false];
+const mixArray = [10, 'kutta', true, 69];

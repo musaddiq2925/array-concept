87f49921c783ffb163d1a067af64472431e3aa3b
nourinCT <nourin.ct@gmail.com>
2025-02-24 00:27:04 +0600

    index array

---
+++ index.js
@@ -0,0 +1,15 @@
+const numbers = [22, 44, 66, 88, 10, 12, 14, 18];
+console.log(numbers);
+
+// get element value by index
+console.log(numbers[0]);
+
+const fourth = numbers[5];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc == 99;
+
+numbers[0] = 999;
+console.log(numbers);

82e81aaf2505e1fdb9c97ebe662a70b81ec669a6
nourinCT <nourin.ct@gmail.com>
2025-03-08 11:25:44 +0600

    pop shift unshift

---
+++ pop.js
@@ -0,0 +1,14 @@
+const friends = ['Pagol', 'Fokir', 'Miskin', 'Atim', 'Gundda'];
+// friends.push('Kamal');
+console.log(friends);
+
+// friends.pop();
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+
+const out1 =friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+console.log(out1, out2);
+console.log(friends);
---
+++ shift-unshift.js
@@ -0,0 +1,8 @@
+const friends = ['Pagol', 'Fokir', 'Miskin', 'Atim', 'Gundda'];
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+friends.unshift('Kamal');
+console.log(friends);
